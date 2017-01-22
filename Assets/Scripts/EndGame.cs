18387de4d28713416826afc0e1e4c3f46c4a64f7
soxies1 <tyson_bulmer@hotmail.com>
2017-01-21 18:55:42 -0800

    Added some graphics to end game screen

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("RestartGame");
+	}
+
+	IEnumerator RestartGame(){
+		yield return new WaitForSeconds(5f);
+		SceneManager.LoadScene(0);
+	}
+	
+}
