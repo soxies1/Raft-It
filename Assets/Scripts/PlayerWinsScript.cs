8210c30fdbb8c09560a54dac8fa51d71b47c5c26
TChan92 <z2x4c6@gmail.com>
2017-01-21 18:54:08 -0800

    end stuff done

--- Assets/Scripts/PlayerWinsScript.cs
+++ Assets/Scripts/PlayerWinsScript.cs
@@ -6,6 +6,18 @@ using UnityEngine.UI;
 public class PlayerWinsScript : MonoBehaviour
 {
 
+    bool player1ScoreHigher()
+    {
+        return int.Parse(GameManager.Instance.Player1ScoreText.text) >
+               int.Parse(GameManager.Instance.Player2ScoreText.text);
+    }
+
+    bool player2ScoreHigher()
+    {
+        return int.Parse(GameManager.Instance.Player1ScoreText.text) <
+               int.Parse(GameManager.Instance.Player2ScoreText.text);
+    }
+
     public Text text;
 	// Use this for initialization
 	void Start () {
@@ -20,6 +32,18 @@ public class PlayerWinsScript : MonoBehaviour
 	    else if (GameManager.player1Lost)
 	    {
 	        text.text = "Player 2 Wins!!";
+	    }
+	    else
+	    {
+	        if (player1ScoreHigher())
+	        {
+	            text.text = "Player 1 Wins!!";
+	        }
+	        else if (player2ScoreHigher())
+	        {
+	            text.text = "Player 2 Wins!!";
+	        }
+	        else text.text = "It's a Tie!!";
 	    }
 	}
 	
