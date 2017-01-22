c50b6f2c67f46453bafabdd6d73ba7b8241922d9
soxies1 <tyson_bulmer@hotmail.com>
2017-01-21 16:17:56 -0800

    Added main menu and changed characters

---
+++ Assets/Scripts/Play.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Play : MonoBehaviour {
+
+	public void PlayClicked(){
+		SceneManager.LoadScene(2);
+	}
+}
